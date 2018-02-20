24dda6455939993a4e8343c44d5109778c017052
patspeiser <pat@socialflow.com>
2018-02-20 00:14:07 -0500

    classjawnin

--- gdax.js
+++ gdax.js
@@ -1,5 +1,22 @@
+const gdax = require('gdax');
+const path = require('path');
+const config = require(path.join(__dirname, 'config'));
+
 class Gdax {
-	constructor(){};
+	constructor(){
+		this.socket = new gdax.WebsocketClient(
+			this.prods,
+			config.websocketUrl,
+			null,
+			['full']
+		);
+	};
+
+	get stream(){
+		this.socket.on('message', data =>{
+			//console.log(data);
+		});
+	};
 }
 
 module.exports = Gdax;
--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const app    = require(path.join(__dirname, 'app'));
 const server = require('http').createServer(app);
 const config = require(path.join(__dirname, 'config')).config;
 const db 	 = require(path.join(__dirname, 'db')).db;
+const Gdax   = require(path.join(__dirname, 'Gdax'));
 const port   = 3037;
+
 class Server{
 	constructor(server){
 		this.server = server;
 	};
 	start(port){
-		console.log('here');
 		this.server.listen(port, ()=>{
 			console.log('on port', port);
 		});
@@ -21,5 +22,7 @@ db.sync()
 .then( db =>{
 	this.server = new Server(server);
 	this.server.start(port);
+	this.gdax   = new Gdax();
+	this.gdax.stream();
 });
 
