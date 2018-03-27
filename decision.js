927e7eb59d84aa651f9d048c662f16fe6af24092
patspeiser <pat@socialflow.com>
2018-03-26 20:52:30 -0400

    correct products and data now

--- decision.js
+++ decision.js
@@ -18,37 +18,26 @@ class Decision {
 			return this.getProducts(this.interval, this.products)
 			.then( prods=>{
 				if(prods){
-					prods.forEach(product=>{
-						var product_id = product[0].product_id;
-						this.product = product.reverse();
-						if(this.product && this.product.length > 0){
-							this.runStrats(this.product, 5).then( data=>{
-								this.data = data;
-								console.log(this.data);
-								this.data.product_id = product_id;
-								this.buffer.insert(this.data);
-								resolve();
-								reject('errDecisionEvaluate');
-							});
-						};
-					});
+					this.runStrats(this.buffer, prods, 1);
 				};
 			});
 		});
 	};
-	runStrats(sets, period){
-		this.period = period;
-		this.sets = sets; 
-		return new Promise( (resolve, reject)=>{
-			if(this.sets && this.sets.length > 0){
-				this.product_id = this.sets[0].product_id;
+	runStrats(buffer, prods, period){
+		var buffer = buffer; 
+		this.prods = prods;
+		this.strats = []; 
+		this.prods.forEach(product=>{
+			this.period = period;
+			this.product = product.reverse();
+			if(this.product && this.product.length > 0){
+				this.product_id = this.product[0].product_id;
 				this.strat = new Strategy('gdax');
 				this.prices   = [];
-				this.sets.map( set =>{
-					this.set = set;
-					this.prices.push(this.set.price);
+				this.product.map( product =>{
+					this.prices.push(product.price);
 				});
-				this.candles = this.createCandles(this.sets, this.period);
+				this.candles = this.createCandles(this.product, this.period, this.product_id);
 				this.highs   = [];
 				this.lows    = [];
 				this.opens   = [];
@@ -73,7 +62,6 @@ class Decision {
 						close: this.closes,
 						volume: this.volumes
 					};
-					this.period = this.priceSets.high.length;
 					this.strategies = {
 						adx: 	this.strat.adx(		this.priceSets, {period: this.period}),
 						atr: 	this.strat.atr(		this.priceSets, {period: this.period}),
@@ -86,81 +74,83 @@ class Decision {
 						stoch: 	this.strat.stoch(	this.priceSets, {kPeriod: 5, kSlowingPeriod: 3 , dPeriod: 3}),
 						ultOsc: this.strat.ultOsc(	this.priceSets, {short:this.period, medium: this.period * 2 , long: this.period * 3})
 					};
-					this.keys = Object.keys(this.strategies);
-					this.values = Object.values(this.strategies);
-					var that = this;
-					return Promise.all(this.values).then(function(data){
-						if(data){
-							resolve({
-								sets     	: that.priceSets, 
-								strategies 	: that.keys, 
-								data       	: data
-							});
-						} else {
-							reject("errDecisionRunStrats")
-						};
+					this.strats.push({
+						product_id: this.product_id,
+						sets:       this.priceSets,
+						strategies: this.strategies
 					});
 				}
 			}
 		});
+		return Promise.all(this.strats.map( s=>{
+			return Promise.all(Object.values(s.strategies)).then( (data)=>{
+				s.data = data;
+				buffer.insert(s);
+			});	
+		})).then(function(data){
+			/*
+				return {
+					sets     	: that.priceSets, 
+					strategies 	: that.keys, 
+					data       	: data
+				};
+				*/
+		});
 	};
-	createCandles(set, period){
+	createCandles(set, period, id){
 		this.set = set;
 		this.period = period;
-		function candle(set, period){
-			this.set = set;
-			this.period = period * 1000 * 60; // period should be minutes for now
-			if(this.set){
-				this.candles = [];
-				this.startTime = new Date(this.set[0].time).getTime();
-				this.endTime   = this.startTime + this.period;
-				this.candle = {
-					high: 	0,
-					low : 	0,
-					open: 	0,
-					close: 	0,
-					volume: 0
-				};
-				while(this.set && this.set.length > 0){
-					this.event = this.set.shift();
-					if(this.event){
-						this.eventTime = new Date(this.event.time).getTime();
-						if(this.eventTime <= this.endTime){
-							this.candle.volume+=1;
-							this.price = this.event.price;
-							if(this.candle.open === 0){
-								this.candle.open = this.price;
-							}
-							if(this.candle.low === 0){
-								this.candle.low = this.price;
-							};
-							if(this.price >= this.candle.high){
-								this.candle.high = this.price;
-							};
-							if(this.price <= this.candle.low){
-								this.candle.low  = this.price;
-							};
-						}  else {
-							this.candle.close = this.price;
-							if(this.candle.volume > 0){
-								this.candles.push(this.candle);
-							}
-							this.candle = {
-								high: 	0,
-								low : 	0,
-								open: 	0,
-								close: 	0,
-								volume: 0
-							}
-							this.startTime = this.endTime;
-							this.endTime   = this.startTime + this.period;
+		this.candles = [];
+		this.set = set;
+		this.period = period * ( 60 * 1000 );
+		if(this.set){
+			this.startTime = new Date(this.set[0].time).getTime();
+			this.endTime   = parseFloat(this.startTime) + parseFloat(this.period);
+			this.candle = {
+				high: 	0,
+				low : 	0,
+				open: 	0,
+				close: 	0,
+				volume: 0
+			};
+			while(this.set && this.set.length > 0){
+				this.event = this.set.shift();
+				if(this.event){
+					this.eventTime = new Date(this.event.time).getTime();
+					if(this.eventTime <= this.endTime){
+						this.candle.volume+=1;
+						this.price = this.event.price;
+						if(this.candle.open === 0){
+							this.candle.open = this.price;
+						}
+						if(this.candle.low === 0){
+							this.candle.low = this.price;
 						};
-					}
-				};
-			};	
-			return this.candles;	
+						if(this.price >= this.candle.high){
+							this.candle.high = this.price;
+						};
+						if(this.price <= this.candle.low){
+							this.candle.low  = this.price;
+						};
+					}  else {
+						this.candle.close = this.price;
+						if(this.candle.volume > 0){
+							this.candles.push(this.candle);
+						}
+						this.candle = {
+							high: 	0,
+							low : 	0,
+							open: 	0,
+							close: 	0,
+							volume: 0
+						}
+						this.startTime = this.endTime;
+						this.endTime   = this.startTime + this.period;
+					};
+				}
+			};
 		};
-		return candle.call(this, this.set, this.period);
+		return this.candles;	
 	};
 	getProducts(interval, productList){
 		this.promises = []; 
@@ -184,7 +174,7 @@ class Decision {
 				data[1].map(d =>{
 					//console.log(chalk.magenta(d.product_id));
 				});
-				return data;
+			return data;
 		});
 	};
 };
